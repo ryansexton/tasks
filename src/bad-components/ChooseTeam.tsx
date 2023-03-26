2fdc36212a374bbbe1fbdb82ac6bd981773d36fa
ryansexton <rjsexton1210@gmail.com>
2023-03-26 15:22:59 -0400

    ChooseTeam

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -14,18 +14,21 @@ export function ChooseTeam(): JSX.Element {
     const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
-        /*
+    function chooseMember(newMember: string) {
+        const newOptions = [...allOptions];
+        const removeMember = newOptions.findIndex(
+            (member: string): boolean => member === newMember
+        );
         if (!team.includes(newMember)) {
-            team.push(newMember);
+            setTeam([...team, newMember]);
+            newOptions.splice(removeMember, 1);
+            setAllOptions(newOptions);
         }
-        */
     }
 
     function clearTeam() {
-        /*
-        team = [];
-        */
+        setTeam([]);
+        setAllOptions(PEOPLE);
     }
 
     return (
@@ -36,7 +39,10 @@ export function ChooseTeam(): JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
