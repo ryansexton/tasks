58e11b4321dfad50310b5594faaabd17c96f4a47
ryansexton <rjsexton1210@gmail.com>
2023-03-28 20:36:20 -0400

    CheckAnswer and GiveAttempts

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [givenAnswer, setGivenAnswer] = useState<string>("");
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group>
+                <Form.Label>Request:</Form.Label>
+                <Form.Control
+                    type="text"
+                    value={givenAnswer}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setGivenAnswer(event.target.value)
+                    }
+                />
+            </Form.Group>
+            <div>{expectedAnswer === givenAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,36 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attempts, updateAttempts] = useState<number>(3);
+    const [requestAttempts, setRequestAttempts] = useState<string>("");
     return (
         <div>
             <h3>Give Attempts</h3>
+            <Button
+                onClick={() => updateAttempts(attempts - 1)}
+                disabled={attempts === 0}
+            >
+                Use
+            </Button>
+            <Button
+                onClick={() =>
+                    updateAttempts(attempts + Number(requestAttempts))
+                }
+            >
+                Gain
+            </Button>
+            <Form.Group>
+                <Form.Label>Request:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={requestAttempts}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setRequestAttempts(event.target.value)
+                    }
+                />
+            </Form.Group>
+            Attempts: {attempts}
         </div>
     );
 }
